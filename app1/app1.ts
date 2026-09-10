32f55277d49158a8cdaf02aa77b25981426f9f33
Ishan Sharma <ishan.spain@gmail.com>
2026-09-10 10:03:30 +0530

    chore: add package.json and pnpm-lock.yaml for app1 and app2 with dependencies

---
+++ app1/app1.ts
@@ -0,0 +1,12 @@
+/* import merge from "lodash/merge";
+console.log("lodash merge function:", merge) ; */
+
+//=======================================
+/* import z from "zod";
+console.log("zod function", z) */
+
+//=======================================
+import mongoose from "mongoose";
+console.log("mongoose funciton", mongoose)
+
+//=======================================
---
+++ app2/app2.ts
@@ -0,0 +1,13 @@
+/* import merge from "lodash/merge";
+console.log("lodash merge function:", merge) ; */
+
+
+//=======================================
+/* import z from "zod";
+console.log("zod function", z) */
+
+//=======================================
+import mongoose from "mongoose";
+console.log("mongoose funciton", mongoose)
+
+//=======================================
